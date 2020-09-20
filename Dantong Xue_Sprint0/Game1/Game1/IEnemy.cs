d8487b673d8b561a7b76de6b001feef5539def1e
Jason Lian <lian.109@buckeyemail.osu.edu>
2020-09-20 01:17:20 -0400

    Add sprites of enemies, npcs and items to the content. Add EnemySpriteFactory class and IEnemy interface.

---
+++ Dantong Xue_Sprint0/Game1/Game1/IEnemy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Game1
+{
+    public interface IEnemy
+    {
+        public void UpdateEnemy();
+        public void DrawEnemy(Vector2 location);
+    }
+}
