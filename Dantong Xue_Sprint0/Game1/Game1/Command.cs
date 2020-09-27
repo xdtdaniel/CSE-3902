95d2e1cb8c768e0d64de69c580bb5e03966f2365
ZH-project <71285597+ZH-project@users.noreply.github.com>
2020-09-27 10:15:35 -0400

    Try to update command to execute item

--- Dantong Xue_Sprint0/Game1/Game1/Command.cs
+++ Dantong Xue_Sprint0/Game1/Game1/Command.cs
@@ -3,6 +3,9 @@ using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Game1.Code.Item.ItemFactory;
+using Game1.Code.Item.ItemInterface;
+using Game1.Code.Item.ItemSprite;
 
 namespace Game1
 {
@@ -11,14 +14,19 @@ namespace Game1
         private static int count;
         
         public enum Actions { non_moving_still, moving_still, non_moving_animated, moving_animated, text, exit, none };
+        public enum ItemSelect { arrow, bomb, boomerang, bow, clock, compass,fairy,heart,heartcontainer,key,map,ruby,triforce};
+        private static ItemSelect current_item; 
 
         private static Actions prev;
         private static Actions curr;
+
 
         public Command()
         {
             prev = Actions.none;
             curr = Actions.non_moving_still;
+
+            current_item = ItemSelect.arrow;
         }
 
         public Actions getPrev()
@@ -29,6 +37,10 @@ namespace Game1
         public Actions getCurr()
         {
             return curr;
+        }
+
+        public ItemSelect getState() { 
+            return current_item;
         }
 
         public void Execute(Actions a, Game1 g, SpriteBatch spriteBatch)
@@ -70,6 +82,55 @@ namespace Game1
 
             spriteBatch.End();
         }
+        public void DisplayItem(ItemSelect i, Game1 g) {
+            System.Diagnostics.Debug.WriteLine(i);
+            //  initial should display item arrow
+            if (current_item == ItemSelect.arrow)
+            {
+                 switch (current_item) // change based on input U or I
+                {
+                    case ItemSelect.bomb:
+                        ItemSpriteFactory.Instance.CreateBomb();
+                        break;
+                    case ItemSelect.boomerang:
+                        ItemSpriteFactory.Instance.CreateBoomerang();
+                        break;
+                    case ItemSelect.bow:
+                        ItemSpriteFactory.Instance.CreateBow();
+                        break;
+                    case ItemSelect.clock:
+                        ItemSpriteFactory.Instance.CreateClock();
+                        break;
+                    case ItemSelect.compass:
+                        ItemSpriteFactory.Instance.CreateCompass();
+                        break;
+                    case ItemSelect.fairy:
+                        ItemSpriteFactory.Instance.CreateFairy();
+                        break;
+                    case ItemSelect.heart:
+                        ItemSpriteFactory.Instance.CreateHeart();
+                        break;
+                    case ItemSelect.heartcontainer:
+                        ItemSpriteFactory.Instance.CreateHeartContainer();
+                        break;
+                    case ItemSelect.key:
+                        ItemSpriteFactory.Instance.CreateKey();
+                        break;
+                    case ItemSelect.map:
+                        ItemSpriteFactory.Instance.CreateMap();
+                        break;
+                    case ItemSelect.ruby:
+                        ItemSpriteFactory.Instance.CreateRuby();
+                        break;
+                    case ItemSelect.triforce:
+                        ItemSpriteFactory.Instance.CreateTriforce();
+                        break;
+                }
+
+                current_item = ItemSelect.arrow;
+            }
+
+         }
 
         private void DrawMovingAnimated(Game1 g, SpriteBatch spriteBatch)
         {
