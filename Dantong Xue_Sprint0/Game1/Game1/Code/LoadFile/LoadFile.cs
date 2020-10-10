da9bb09ff1b2fcf9df04fa1b1a98ec2356acde37
ZH-project <71285597+ZH-project@users.noreply.github.com>
2020-10-10 02:25:40 -0400

    Update Load File for sprint 3

--- Dantong Xue_Sprint0/Game1/Game1/Code/LoadFile/LoadFile.cs
+++ Dantong Xue_Sprint0/Game1/Game1/Code/LoadFile/LoadFile.cs
@@ -6,6 +6,7 @@ using System.Windows.Forms;
 using System.DirectoryServices.ActiveDirectory;
 using Game1.Code.Block.BlockFactory;
 using System.Drawing;
+using Microsoft.Xna.Framework;
 
 namespace Game1.Code.LoadFile
 {
@@ -44,21 +45,36 @@ namespace Game1.Code.LoadFile
                 cell_y++;
 
             }
-
+            Vector2 location;
+            
             //switch case for each string
             for (int index = 0;index < mapElementList.Count; index++)
             {
+                location.X = mapElementList[index].Item1;
+                location.Y = mapElementList[index].Item2;
                 switch (mapElementList[index].Item3)
                 {
                     case "black": 
-                        BlockFactory.Instance.CreateBlackBlock();// need to pass row anad columbs and multiply by the block's width and height?
+                        BlockFactory.Instance.CreateBlackBlock(location);// need to pass row anad columbs and multiply by the block's width and height?
                         break;
                     case "block":
-                        BlockFactory.Instance.CreateFlatBlock();
-                         break;
+                        BlockFactory.Instance.CreateFlatBlock(location);
+                        break;
                     case "dragon":
-                        BlockFactory.Instance.CreateDragon();
-                         break;
+                        BlockFactory.Instance.CreateDragon(location);
+                        break;
+                    case "dragonBlue":
+                        BlockFactory.Instance.CreateBlueDragon(location);
+                        break;
+                    case "fire":
+                        BlockFactory.Instance.CreateFire(location);
+                        break;
+                    case "fish":
+                        BlockFactory.Instance.CreateFish(location);
+                        break;
+                    case "fishBlue":
+                        BlockFactory.Instance.CreateBlueFish(location);
+                        break;
                 }
 
             }
