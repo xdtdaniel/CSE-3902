de170a10d5a39cdbbf38ee686950ca851370ff02
Jason Lian <lian.109@buckeyemail.osu.edu>
2020-10-24 14:36:06 -0400

    Remove the testing code.

--- Dantong Xue_Sprint0/Game1/Game1/Game1.cs
+++ Dantong Xue_Sprint0/Game1/Game1/Game1.cs
@@ -129,8 +129,6 @@ namespace Game1
             // enemyKeyboradController.Draw(_spriteBatch);
             playerPanel.PlayerDraw();
 
-            _spriteBatch.DrawString(_spriteFont, EnemyLoader.NoEnemy().ToString(), new Vector2(400, 200), Color.Red);
-
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             _spriteBatch.End();
