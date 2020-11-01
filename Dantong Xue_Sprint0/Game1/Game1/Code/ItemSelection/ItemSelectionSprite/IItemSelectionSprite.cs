492016f1159627a3044f3c8d4aaa70d276811720
ZH-project <71285597+ZH-project@users.noreply.github.com>
2020-11-01 16:10:59 -0500

    Add ItemSelection part

---
+++ Dantong Xue_Sprint0/Game1/Game1/Code/ItemSelection/ItemSelectionSprite/IItemSelectionSprite.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game1.Code.ItemSelection.ItemSelectionSprite
+{
+    public interface IItemSelectionSprite
+    {
+        void DrawSelectionScreen(SpriteBatch spriteBatch);
+        void UpdateSelectionScreen();
+    }
+}
