e46eb4c76cfa817151ad1429fc25dc4a9e767ae7
ZH-project <71285597+ZH-project@users.noreply.github.com>
2020-11-01 21:24:53 -0500

    Add HUD Factory class

---
+++ Dantong Xue_Sprint0/Game1/Game1/Code/HUD/hudFactory/HUDFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Game1.Code.Item.ItemSprite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Game1.Player;
+using Microsoft.Xna.Framework;
+namespace Game1.Code.HUD.hudFactory
+{
+	public static class HUDFactory
+	{
+
+		private static Texture2D hudFrame;
+
+		public static void LoadAllHUDTextures(ContentManager content)
+		{			
+			hudFrame = content.Load<Texture2D>("HUD/HUD_frame");
+
+		}
+
+
+		public static Texture2D LoadHUDFrame()
+		{
+
+			return hudFrame;
+		}
+
+	}
+}
