10781d4e3a97a9a854ea554524cdf4ee772dede8
ZH-project <71285597+ZH-project@users.noreply.github.com>
2020-11-07 18:00:44 -0500

    Revert "Add files via upload"
    This reverts commit af5066f8669e38859722b998e6becab2c14d6aa2.

--- Dantong Xue_Sprint0/Game1/Game1/Code/HUD/Sprite/HUDNumberOfRuby.cs
+++ Dantong Xue_Sprint0/Game1/Game1/Code/HUD/Sprite/HUDNumberOfRuby.cs
@@ -48,9 +48,6 @@ namespace Game1.Code.HUD.Sprite
         {
             x = (int)newStartX + 104 * scale;
             y = (int)newStartY - 56 * scale + 16 * scale;
-
-            numberOfRuby = HUDFactory.LoadNumber(hudItemList["Ruby"]);
-
         }
 
     }
