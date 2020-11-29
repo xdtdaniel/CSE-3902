7962f4bfe36653ff9a0c2d9e7e162166cf0bfda8
ZH-project <71285597+ZH-project@users.noreply.github.com>
2020-11-29 12:14:30 -0500

    Add files via upload

---
+++ Dantong Xue_Sprint0/Game1/Game1/Code/HUD/Sprite/HUDExp.cs
@@ -0,0 +1,71 @@
+﻿using Game1.Code.HUD.Factory;
+using Game1.Code.LoadFile;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Text;
+namespace Game1.Code.HUD.Sprite
+{
+    public class HUDExp : IHUDSprite
+    {
+        private Texture2D exp;
+
+        private static int scale = (int)LoadAll.Instance.scale;
+        private int height = 6 * scale;
+        private int width = 4  * scale;
+        private int x;
+        private int y;
+        private int maxExpPerLine = 16;
+        private int expCount;
+
+        private int preX = 172 * scale;
+        private int preY = -28 * scale;
+        private int level = 0;
+
+        Rectangle sourceRectangle;
+        Rectangle destinationRectangle;
+
+        public HUDExp(int killedEnemies)
+        {
+            expCount = killedEnemies;
+            //initial values
+            exp = HUDFactory.LoadExpPattern();   
+            sourceRectangle = new Rectangle();
+            destinationRectangle = new Rectangle();
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+            for (int i = 0; i < expCount; i++)
+            {
+                if (expCount % maxExpPerLine!=0 && level!=8)
+                {
+                    sourceRectangle = new Rectangle(0, 0, exp.Width, exp.Height);
+                    destinationRectangle = new Rectangle(x + i * width, y, width, height);
+                    spriteBatch.Draw(exp, destinationRectangle, sourceRectangle, Color.White);
+                }
+                else if (expCount % maxExpPerLine == 0 && level != 8 )
+                {
+                    i = 0;
+                    sourceRectangle = new Rectangle(0, 0, exp.Width, exp.Height);
+                    destinationRectangle = new Rectangle(x + i * width, y, width, height);
+                    spriteBatch.Draw(exp, destinationRectangle, sourceRectangle, Color.White);
+                    level++;
+                }
+                else {
+                    break;
+                }
+            }
+        }
+        public void Update(float newStartX, float newStartY)
+        {
+            x = (int)newStartX + preX;
+            y = (int)newStartY + preY;
+            expCount = Enemy.DrawAndUpdateEnemy.numberOfKilled();
+        }
+    }
+}
+
